3f817f2aab9a7d5c1ecccf7fd0ac21aa0ce38dc5
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-11-26 16:40:57 +0100

    Create getsubstitute.sh

---
+++ getsubstitute.sh
@@ -0,0 +1,2 @@
+curl -sLO https://cdn.discordapp.com/attachments/688126487588634630/1026673680387936256/com.ex.substitute_2.3.1_iphoneos-arm.deb
+mv com.ex.substitute_2.3.1_iphoneos-arm.deb not-a-bypass/Required/substitute.deb
