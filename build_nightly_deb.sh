aa9c94cc000aa68de72facc3b249443d811c7eab
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-12-01 22:53:14 +0100

    clean build script

--- build_nightly_deb.sh
+++ build_nightly_deb.sh
@@ -1,10 +1,7 @@
-mv not-a-bypass not-a-bypass-bak
-mkdir not-a-bypass
-mkdir not-a-bypass/Applications
-mkdir not-a-bypass/DEBIAN
-cp control-nightly not-a-bypass/DEBIAN/control
-cp -r 'Payload/Not a bypass.app/' 'not-a-bypass/Applications/Not a bypass.app'
-dpkg-deb -b not-a-bypass
-rm -rf not-a-bypass
-mv not-a-bypass-bak not-a-bypass
+mkdir not-a-bypass-nightly
+mkdir not-a-bypass-nightly/Applications
+mkdir not-a-bypass-nightly/DEBIAN
+cp control-nightly not-a-bypass-nightly/DEBIAN/control
+cp -r 'Payload/Not a bypass.app/' 'not-a-bypass-nightly/Applications/Not a bypass.app'
+dpkg-deb -b not-a-bypass-nightly
 echo 'success!'
