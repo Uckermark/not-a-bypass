e70ab49b34e14e5d782f6097f33c2b25b24558c9
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-12-08 00:00:06 +0100

    Update build_nightly_deb.sh

--- build_nightly_deb.sh
+++ build_nightly_deb.sh
@@ -1,9 +1,7 @@
-mkdir not-a-bypass-nightly
-mkdir not-a-bypass-nightly/Applications
-mkdir not-a-bypass-nightly/DEBIAN
+mkdir not-a-bypass-nightly not-a-bypass-nightly/DEBIAN not-a-bypass-nightly/Applications
 cp control-nightly not-a-bypass-nightly/DEBIAN/control
 cp -r 'Payload/Not a bypass.app/' 'not-a-bypass-nightly/Applications/Not a bypass.app'
-dpkg-deb -b not-a-bypass-nightly
+dpkg-deb -b not-a-bypass-nightly >> /dev/null
 rm -rf not-a-bypass-nightly
-echo 'success!'
+echo 'written output to not-a-bypass-nightly.deb'
 
