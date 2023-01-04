8f06eb42675d08362ba646f92d3c69b8b5cacc9b
Uckermark <93996544+Uckermark@users.noreply.github.com>
2023-01-04 14:47:01 +0100

    add screen for unsupported platforms

---
+++ not-a-bypass/BypassView.swift
@@ -0,0 +1,20 @@
+//
+//  BypassView.swift
+//  not-a-bypass
+//
+//  Created by Leonard Lausen on 04.01.23.
+//
+
+import SwiftUI
+
+struct BypassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BypassView_Previews: PreviewProvider {
+    static var previews: some View {
+        BypassView()
+    }
+}
---
+++ not-a-bypass/UnsupportedView.swift
@@ -0,0 +1,20 @@
+//
+//  UnsupportedView.swift
+//  not-a-bypass
+//
+//  Created by Leonard Lausen on 04.01.23.
+//
+
+import SwiftUI
+
+struct UnsupportedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UnsupportedView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnsupportedView()
+    }
+}
