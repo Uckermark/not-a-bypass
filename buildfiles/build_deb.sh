1860d86965ff49da6b40dc69faff948907e272ff
Uckermark <93996544+Uckermark@users.noreply.github.com>
2023-01-04 15:14:51 +0100

    add files for stable build

---
+++ buildfiles/build_deb.sh
@@ -0,0 +1,7 @@
+mkdir build build/DEBIAN build/Applications
+cp buildfiles/control build/DEBIAN/control
+cp -r 'Payload/Not a bypass.app/' 'build/Applications/not-a-bypass.app'
+dpkg-deb -b build not-a-bypass.deb
+rm -rf build
+echo 'written output to not-a-bypass.deb'
+
